b495749aed3db018b1989a683fc791063356f4b3
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-28 17:42:09 -0400

    currently makes a filter of a dark filter

--- main.c
+++ main.c
@@ -18,16 +18,25 @@ int main() {
 
     printf("Loaded Image with a width of %dpx, a height of %dpx and %d channels.\n", width, height, channels);
 
-    stbi_write_png("sky2.png", width, height, channels, image, width * channels);
-    stbi_write_png("sky.jpg", width, height, channels, image, width * channels);
-    stbi_write_png("sky2.jpg", width, height, channels, image, width * channels);
-
     size_t image_size = width * height * channels;
-    for (long i=0; i<image_size; i++) {
-        if (image[i] != 254 && image[i] != 255) {
-            printf("%d\n", image[i]);
-        }
+    for (long i=0; i<image_size-9; i+=9) {
+        int one = image[i];
+        int two = image[i+1];
+        int three = image[i+2];
+        int four = image[i+2];
+        int five = image[i+2];
+        int six = image[i+2];
+        int seven = image[i+2];
+        int eight = image[i+2];
+        int nine = image[i+2];
+        int ten = image[i+2];
+        int average = (one + two + three + four + five + six + seven + eight + nine + ten) / 10;
+        image[i] = 0;
     }
+
+    stbi_write_png("sky2.png", width, height, channels, image, width * channels);
+    // stbi_write_png("sky.jpg", width, height, channels, image, width * channels);
+    // stbi_write_png("sky2.jpg", width, height, channels, image, width * channels);
 
     stbi_image_free(image);
     return 0;
