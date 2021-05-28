f3dec0c15f25a15db8c05f386d1df1b518b8d174
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-28 17:51:10 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Image Compression Tool
 
+## Description: 
+This tool can create filters for given images and works by taking an image and storing
+image pixels in an array allowing pixel manipulations to be made to allow filters.
+
+### Supported image extensions:
+### - jpg
+### - png
+
+
 - use sobel operators
 
 - this can convert png <=> jpg
--- main.c
+++ main.c
@@ -19,20 +19,46 @@ int main() {
     printf("Loaded Image with a width of %dpx, a height of %dpx and %d channels.\n", width, height, channels);
 
     size_t image_size = width * height * channels;
-    for (long i=0; i<image_size-9; i+=9) {
-        int one = image[i];
-        int two = image[i+1];
-        int three = image[i+2];
-        int four = image[i+2];
-        int five = image[i+2];
-        int six = image[i+2];
-        int seven = image[i+2];
-        int eight = image[i+2];
-        int nine = image[i+2];
-        int ten = image[i+2];
-        int average = (one + two + three + four + five + six + seven + eight + nine + ten) / 10;
-        image[i] = 0;
-    }
+
+    // dark filter
+    // for (long i=0; i<image_size-9; i+=9) {
+    //     int one = image[i];
+    //     int two = image[i+1];
+    //     int three = image[i+2];
+    //     int four = image[i+2];
+    //     int five = image[i+2];
+    //     int six = image[i+2];
+    //     int seven = image[i+2];
+    //     int eight = image[i+2];
+    //     int nine = image[i+2];
+    //     int ten = image[i+2];
+    //     int average = (one + two + three + four + five + six + seven + eight + nine + ten) / 10;
+    //     image[i] = 0;
+    // }
+
+    // greyish and slighly blurrier
+    // for (long i=0; i<image_size-9; i+=9) {
+    //     int one = image[i];
+    //     int two = image[i+1];
+    //     int three = image[i+2];
+    //     int four = image[i+3];
+    //     int five = image[i+4];
+    //     int six = image[i+5];
+    //     int seven = image[i+6];
+    //     int eight = image[i+7];
+    //     int nine = image[i+8];
+    //     int ten = image[i+9];
+    //     int average = (one + two + three + four + five + six + seven + eight + nine + ten) / 10;
+    //     image[i] = average;
+    //     image[i+1] = average;
+    //     image[i+2] = average;
+    //     image[i+3] = average;
+    //     image[i+4] = average;
+    //     image[i+5] = average;
+    //     image[i+6] = average;
+    //     image[i+7] = average;
+    //     image[i+8] = average;
+    // }
 
     stbi_write_png("sky2.png", width, height, channels, image, width * channels);
     // stbi_write_png("sky.jpg", width, height, channels, image, width * channels);
