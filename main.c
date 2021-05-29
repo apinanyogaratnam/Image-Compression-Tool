f0a2dd594adfa850bfc5ab89bdd43967170de1b7
Apinan Yogaratnam <apinanapinan@icloud.com>
2021-05-29 19:05:12 -0400

    output comes but incorrect output

--- main.c
+++ main.c
@@ -20,8 +20,27 @@ int main() {
 
     printf("Loaded Image with a width of %dpx, a height of %dpx and %d channels.\n", width, height, channels);
     size_t size_of_image = width * height * channels;
+
+
+
+    unsigned char image_test[width*2][height*2];
+
+    long width_count = 0;
+    long height_count=0;
+    for (long i=0; i<size_of_image; i++) {
+        image_test[width_count][height_count] = image[i];
+
+        width_count++;
+
+        if (width_count == width*2) {
+            width_count = 0;
+            height_count++;
+        }
+    }
     
-    stbi_write_png("opaque_test.png", width, height, channels, image, width * channels);
+    stbi_write_png("opaque_test.png", width, height, channels, image_test, width * channels);
+
+
 
 
     // stbi_write_png("sky.jpg", width, height, channels, image, width * channels);
